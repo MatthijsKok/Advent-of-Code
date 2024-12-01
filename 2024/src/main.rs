1bd3324427723062356925262bc67f4eb1fc5ba4
Matthijs Kok <m.kok@zanders.eu>
2024-12-01 14:08:10 +0100

    tracing

--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -3,5 +3,11 @@ mod day01;
 fn main() {
     println!("Hello, world!");
 
+    let tracing_subscriber = tracing_subscriber::FmtSubscriber::builder()
+        .with_max_level(tracing::Level::TRACE)
+        .finish();
+    tracing::subscriber::set_global_default(tracing_subscriber)
+        .expect("setting default subscriber failed");
+
     day01::solve();
 }
