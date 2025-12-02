4613f99d94cd7f08aa93fdb386d85bf19c6d3bcc
Matthijs Kok <Matthijs.Kok@rabobank.nl>
2025-12-02 20:01:42 +0100

    scaffold day3

---
+++ 2025/rust/src/day03.rs
@@ -0,0 +1,9 @@
+#[tracing::instrument(skip_all)]
+pub(crate) fn solve_part1(input: &str) -> usize {
+    todo!()
+}
+
+#[tracing::instrument(skip_all)]
+pub(crate) fn solve_part2(input: &str) -> usize {
+    todo!()
+}
