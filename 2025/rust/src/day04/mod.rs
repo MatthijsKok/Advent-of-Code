2e31ef772eda63f07bf59e999ca231a7942a71df
Matthijs Kok <Matthijs.Kok@rabobank.nl>
2025-12-05 15:43:04 +0100

    pass 3 beautiful before fucking it up with an array

--- 2025/rust/src/day04/mod.rs
+++ 2025/rust/src/day04/mod.rs
@@ -9,7 +9,6 @@ const NEIGHBOUR_THRESHOLD: u8 = 5;
 #[tracing::instrument(skip_all, ret)]
 pub(crate) fn solve_part1(input: &str) -> usize {
     // Answer = 1474
-    let mut count: usize = 0;
     let dim_size = input.lines().next().unwrap().len() as isize;
 
     let lookup = input
@@ -22,25 +21,18 @@ pub(crate) fn solve_part1(input: &str) -> usize {
                 .collect::<Vec<_>>()
         })
         .collect::<BTreeMap<_, _>>();
-    for (&(i, j), &val) in &lookup {
-        if val == 0 {
-            continue;
-        }
-        let mut local_count: u8 = 0;
-        for ni in 0.max(i - 1)..=(dim_size - 1).min(i + 1) {
-            for nj in 0.max(j - 1)..=(dim_size - 1).min(j + 1) {
-                // NOTE: Don't need to do check, just bumped Neighbour Threshold by 1
-                // if i == ni && j == nj {
-                //     continue;
-                // }
-                local_count += *(lookup.get(&(ni, nj)).unwrap());
-            }
-        }
-        if local_count < NEIGHBOUR_THRESHOLD {
-            count += 1;
-        }
-    }
-    count
+
+    lookup
+        .iter()
+        .filter(|&(_, &v)| v != 0)
+        .map(|(&(i, j), _)| -> u8 {
+            (0.max(i - 1)..=(dim_size - 1).min(i + 1))
+                .flat_map(|ni| (0.max(j - 1)..=(dim_size - 1).min(j + 1)).map(move |nj| (ni, nj)))
+                .filter_map(|coord| lookup.get(&coord))
+                .sum()
+        })
+        .filter(|&n| n < NEIGHBOUR_THRESHOLD)
+        .count()
 }
 
 #[tracing::instrument(skip_all, ret)]
@@ -63,22 +55,13 @@ pub(crate) fn solve_part2(input: &str) -> usize {
     lookup
         .iter()
         .filter(|&(_, &v)| v != 0)
-        .map(|(&(i, j), _)| {
-            let mut neighbours_count: u8 = 0;
-            for ni in 0.max(i - 1)..=(dim_size - 1).min(i + 1) {
-                for nj in 0.max(j - 1)..=(dim_size - 1).min(j + 1) {
-                    // NOTE: Don't need to do check, just bump Neighbour Threshold by 1
-                    // if i == ni && j == nj {
-                    //     continue;
-                    // }
-                    neighbours_count += *(lookup.get(&(ni, nj)).unwrap());
-                }
-            }
-            neighbours_count
+        .map(|(&(i, j), _)| -> u8 {
+            (0.max(i - 1)..=(dim_size - 1).min(i + 1))
+                .flat_map(|ni| (0.max(j - 1)..=(dim_size - 1).min(j + 1)).map(move |nj| (ni, nj)))
+                .filter_map(|coord| lookup.get(&coord))
+                .sum()
         })
-        // Neighbour Count Threshold
-        // .filter(|&n| n < 4)
-        .filter(|&n| n < 5)
+        .filter(|&n| n < NEIGHBOUR_THRESHOLD)
         .count()
 }
 
