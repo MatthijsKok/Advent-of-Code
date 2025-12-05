e6cafc1912946cab67ffd7e10024035dab939edc
Matthijs Kok <Matthijs.Kok@rabobank.nl>
2025-12-05 15:30:58 +0100

    pass2

--- 2025/rust/src/day04/mod.rs
+++ 2025/rust/src/day04/mod.rs
@@ -2,13 +2,15 @@ use std::collections::{BTreeMap, HashMap};
 
 use rayon::prelude::*;
 
+// Assignment states 4 neighbours as threshold. If we include yourself,
+// we do not need to branch inside the hot loop.
+const NEIGHBOUR_THRESHOLD: u8 = 5;
+
 #[tracing::instrument(skip_all, ret)]
 pub(crate) fn solve_part1(input: &str) -> usize {
     // Answer = 1474
     let mut count: usize = 0;
-
-    let size_i = input.lines().count() as isize;
-    let size_j = input.lines().next().unwrap().len() as isize;
+    let dim_size = input.lines().next().unwrap().len() as isize;
 
     let lookup = input
         .lines()
@@ -25,18 +27,16 @@ pub(crate) fn solve_part1(input: &str) -> usize {
             continue;
         }
         let mut local_count: u8 = 0;
-        for ni in 0.max(i - 1)..=(size_i - 1).min(i + 1) {
-            for nj in 0.max(j - 1)..=(size_j - 1).min(j + 1) {
-                if i == ni && j == nj {
-                    continue;
-                }
-                let nval = *(lookup.get(&(ni, nj)).unwrap());
-                if nval == 1 {
-                    local_count += 1;
-                }
+        for ni in 0.max(i - 1)..=(dim_size - 1).min(i + 1) {
+            for nj in 0.max(j - 1)..=(dim_size - 1).min(j + 1) {
+                // NOTE: Don't need to do check, just bumped Neighbour Threshold by 1
+                // if i == ni && j == nj {
+                //     continue;
+                // }
+                local_count += *(lookup.get(&(ni, nj)).unwrap());
             }
         }
-        if local_count < 4 {
+        if local_count < NEIGHBOUR_THRESHOLD {
             count += 1;
         }
     }
@@ -48,10 +48,9 @@ pub(crate) fn solve_part2(input: &str) -> usize {
     // Answer = ?
     let mut count: usize = 0;
 
-    let size_i = input.lines().count() as isize;
-    let size_j = input.lines().next().unwrap().len() as isize;
+    let dim_size = input.lines().next().unwrap().len() as isize;
 
-    let lookup = input
+    let lookup: BTreeMap<(isize, isize), u8> = input
         .lines()
         .enumerate()
         .flat_map(|(i, line)| {
@@ -60,41 +59,27 @@ pub(crate) fn solve_part2(input: &str) -> usize {
                 .map(|(j, val)| ((i as isize, j as isize), (val == '@') as u8))
                 .collect::<Vec<_>>()
         })
-        .collect::<BTreeMap<_, _>>();
-    for (&(i, j), &val) in &lookup {
-        if val == 0 {
-            continue;
-        }
-        let mut local_count: u8 = 0;
-        for ni in 0.max(i - 1)..=(size_i - 1).min(i + 1) {
-            for nj in 0.max(j - 1)..=(size_j - 1).min(j + 1) {
-                if i == ni && j == nj {
-                    continue;
+        .collect();
+    lookup
+        .iter()
+        .filter(|&(_, &v)| v != 0)
+        .map(|(&(i, j), _)| {
+            let mut neighbours_count: u8 = 0;
+            for ni in 0.max(i - 1)..=(dim_size - 1).min(i + 1) {
+                for nj in 0.max(j - 1)..=(dim_size - 1).min(j + 1) {
+                    // NOTE: Don't need to do check, just bump Neighbour Threshold by 1
+                    // if i == ni && j == nj {
+                    //     continue;
+                    // }
+                    neighbours_count += *(lookup.get(&(ni, nj)).unwrap());
                 }
-                let nval = *(lookup.get(&(ni, nj)).unwrap());
-                local_count += nval;
             }
-        }
-        if local_count < 4 {
-            count += 1;
-        }
-    }
-    count
-
-    // let a = input
-    //     .lines()
-    //     .enumerate()
-    //     .flat_map(|(i, line)| {
-    //         line.chars()
-    //             .enumerate()
-    //             .map(|(j, val)| ((i as isize, j as isize), val))
-    //             .collect::<Vec<_>>()
-    //     })
-    //     .filter(|(coords, val)| *val != '.')
-    //     .map(|((i, j), val)| {
-    //         //
-    //     });
-    // 0
+            neighbours_count
+        })
+        // Neighbour Count Threshold
+        // .filter(|&n| n < 4)
+        .filter(|&n| n < 5)
+        .count()
 }
 
 #[test]
@@ -116,4 +101,19 @@ fn part1_examples() {
 }
 
 #[test]
-fn part2_examples() {}
+fn part2_examples() {
+    let example = [
+        "..@@.@@@@.",
+        "@@@.@.@.@@",
+        "@@@@@.@.@@",
+        "@.@@@@..@.",
+        "@@.@@@@.@@",
+        ".@@@@@@@.@",
+        ".@.@.@.@@@",
+        "@.@@@.@@@@",
+        ".@@@@@@@@.",
+        "@.@.@@@.@.",
+    ]
+    .join("\n");
+    assert_eq!(solve_part2(&example), 13);
+}
