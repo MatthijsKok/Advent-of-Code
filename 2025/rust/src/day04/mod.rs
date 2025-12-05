b5b91ba8ebe8ece69a2432e081d921e484b341fa
Matthijs Kok <Matthijs.Kok@rabobank.nl>
2025-12-05 13:53:28 +0100

    optimization pass 1

--- 2025/rust/src/day04/mod.rs
+++ 2025/rust/src/day04/mod.rs
@@ -16,28 +16,22 @@ pub(crate) fn solve_part1(input: &str) -> usize {
         .flat_map(|(i, line)| {
             line.chars()
                 .enumerate()
-                .map(|(j, val)| ((i as isize, j as isize), val))
+                .map(|(j, val)| ((i as isize, j as isize), (val == '@') as u8))
                 .collect::<Vec<_>>()
         })
         .collect::<BTreeMap<_, _>>();
     for (&(i, j), &val) in &lookup {
-        if val == '.' {
+        if val == 0 {
             continue;
         }
         let mut local_count: u8 = 0;
-        for ni in i - 1..=i + 1 {
-            if ni < 0 || ni >= size_i {
-                continue;
-            }
-            for nj in j - 1..=j + 1 {
-                if nj < 0 || nj >= size_j {
-                    continue;
-                }
+        for ni in 0.max(i - 1)..=(size_i - 1).min(i + 1) {
+            for nj in 0.max(j - 1)..=(size_j - 1).min(j + 1) {
                 if i == ni && j == nj {
                     continue;
                 }
                 let nval = *(lookup.get(&(ni, nj)).unwrap());
-                if nval == '@' {
+                if nval == 1 {
                     local_count += 1;
                 }
             }
@@ -52,7 +46,55 @@ pub(crate) fn solve_part1(input: &str) -> usize {
 #[tracing::instrument(skip_all, ret)]
 pub(crate) fn solve_part2(input: &str) -> usize {
     // Answer = ?
-    0
+    let mut count: usize = 0;
+
+    let size_i = input.lines().count() as isize;
+    let size_j = input.lines().next().unwrap().len() as isize;
+
+    let lookup = input
+        .lines()
+        .enumerate()
+        .flat_map(|(i, line)| {
+            line.chars()
+                .enumerate()
+                .map(|(j, val)| ((i as isize, j as isize), (val == '@') as u8))
+                .collect::<Vec<_>>()
+        })
+        .collect::<BTreeMap<_, _>>();
+    for (&(i, j), &val) in &lookup {
+        if val == 0 {
+            continue;
+        }
+        let mut local_count: u8 = 0;
+        for ni in 0.max(i - 1)..=(size_i - 1).min(i + 1) {
+            for nj in 0.max(j - 1)..=(size_j - 1).min(j + 1) {
+                if i == ni && j == nj {
+                    continue;
+                }
+                let nval = *(lookup.get(&(ni, nj)).unwrap());
+                local_count += nval;
+            }
+        }
+        if local_count < 4 {
+            count += 1;
+        }
+    }
+    count
+
+    // let a = input
+    //     .lines()
+    //     .enumerate()
+    //     .flat_map(|(i, line)| {
+    //         line.chars()
+    //             .enumerate()
+    //             .map(|(j, val)| ((i as isize, j as isize), val))
+    //             .collect::<Vec<_>>()
+    //     })
+    //     .filter(|(coords, val)| *val != '.')
+    //     .map(|((i, j), val)| {
+    //         //
+    //     });
+    // 0
 }
 
 #[test]
