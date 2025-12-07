5b71c55c486fdc0c2dcf4258d99ae9b864d171cd
Matthijs Kok <Matthijs.Kok@rabobank.nl>
2025-12-07 20:09:43 +0100

    day 6 part 2

--- 2025/rust/src/day06/mod.rs
+++ 2025/rust/src/day06/mod.rs
@@ -41,8 +41,66 @@ pub(crate) fn solve_part1(input: &str) -> usize {
 
 #[tracing::instrument(skip_all, ret)]
 pub(crate) fn solve_part2(input: &str) -> usize {
-    // Answer = ?
-    0
+    // Answer = 9630000828442
+    let input_width = input.lines().next().unwrap().len();
+    assert_eq!(input_width, 3746);
+    assert!(input.lines().all(|line| line.len() == 3746));
+    let input_height = input.lines().count();
+    let char_grid = input
+        .lines()
+        .map(|line| line.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    // let rotated_grid: Vec<Vec<char>> = (0..input_width)
+    //     .rev()
+    //     .map(|col| (0..input_height).map(|row| char_grid[row][col]).collect())
+    //     .collect();
+
+    // let rotated_lines = rotated_grid
+    //     .into_iter()
+    //     .map(|line| line.iter().collect::<String>())
+    //     .collect::<Vec<_>>();
+    // // dbg!(&rotated_lines);
+    // rotated_lines
+    //     .split(|line| line.trim().is_empty())
+    //     .filter(|&chunk| !chunk.is_empty())
+    //     .map(|chunk| -> usize {
+    //         //
+
+    //         0
+    //     })
+    //     .sum()
+
+    (0..input_width)
+        .rev()
+        .map(|col| {
+            (0..input_height)
+                .map(|row| char_grid[row][col])
+                .collect::<String>()
+        })
+        .collect::<Vec<_>>()
+        .split(|line| line.trim().is_empty())
+        .filter(|&chunk| !chunk.is_empty())
+        .map(|chunk| -> usize {
+            let operator = chunk
+                .iter()
+                .flat_map(|line| line.chars())
+                .find(|&char| char == '+' || char == '*')
+                .unwrap();
+            let numbers = chunk.iter().map(|line| {
+                line.chars()
+                    .filter(|char| char.is_ascii_digit())
+                    .collect::<String>()
+                    .parse::<usize>()
+                    .unwrap()
+            });
+            match operator {
+                '*' => numbers.product(),
+                '+' => numbers.sum(),
+                _ => panic!(),
+            }
+        })
+        .sum()
 }
 
 #[test]
