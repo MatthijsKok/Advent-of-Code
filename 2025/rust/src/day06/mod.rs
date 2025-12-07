44cf135f38e6d86199194f9f7bd93104ad6c1f3a
Matthijs Kok <Matthijs.Kok@rabobank.nl>
2025-12-07 18:29:13 +0100

    day 6 part 1

--- 2025/rust/src/day06/mod.rs
+++ 2025/rust/src/day06/mod.rs
@@ -1,7 +1,42 @@
+use std::{array, panic};
+
+use rayon::str::ParallelString;
+
 #[tracing::instrument(skip_all, ret)]
 pub(crate) fn solve_part1(input: &str) -> usize {
-    // Answer = ?
-    0
+    // Answer = 5977759036837
+    let input_width = input
+        .lines()
+        .next()
+        .unwrap()
+        .split_ascii_whitespace()
+        .count();
+    assert_eq!(input_width, 1000);
+
+    let mut data: [Vec<&str>; 1000] = array::repeat(Vec::new());
+
+    for line in input.lines() {
+        for (i, item) in line.split_ascii_whitespace().enumerate() {
+            if let Some(data_item) = data.get_mut(i) {
+                data_item.push(item);
+            }
+        }
+    }
+
+    data.iter_mut()
+        .map(|item| -> usize {
+            let sign = item.pop().unwrap();
+            let numbers: Vec<usize> = item
+                .iter()
+                .map(|n| n.parse::<usize>().unwrap())
+                .collect::<Vec<_>>();
+            match sign {
+                "*" => numbers.iter().product(),
+                "+" => numbers.iter().sum(),
+                _ => panic!(),
+            }
+        })
+        .sum()
 }
 
 #[tracing::instrument(skip_all, ret)]
--- 2025/rust/src/main.rs
+++ 2025/rust/src/main.rs
@@ -24,53 +24,53 @@ fn main() {
     tracing::subscriber::set_global_default(tracing_subscriber).unwrap();
     info!("Hello, Advent of Code!");
 
-    let input = include_str!("day01/input.txt");
-    day01::solve_part1(input);
-    day01::solve_part2(input);
+    // let input = include_str!("day01/input.txt");
+    // day01::solve_part1(input);
+    // day01::solve_part2(input);
 
-    let input = include_str!("day02/input.txt");
-    day02::solve_part1(input);
-    day02::solve_part2(input);
+    // let input = include_str!("day02/input.txt");
+    // day02::solve_part1(input);
+    // day02::solve_part2(input);
 
-    let input = include_str!("day03/input.txt");
-    day03::solve_part1(input);
-    day03::solve_part2(input);
+    // let input = include_str!("day03/input.txt");
+    // day03::solve_part1(input);
+    // day03::solve_part2(input);
 
-    let input = include_str!("day04/input.txt");
-    day04::solve_part1(input);
-    day04::solve_part2(input);
+    // let input = include_str!("day04/input.txt");
+    // day04::solve_part1(input);
+    // day04::solve_part2(input);
 
-    let input = include_str!("day05/input.txt");
-    day05::solve_part1(input);
-    day05::solve_part2(input);
+    // let input = include_str!("day05/input.txt");
+    // day05::solve_part1(input);
+    // day05::solve_part2(input);
 
     let input = include_str!("day06/input.txt");
     day06::solve_part1(input);
     day06::solve_part2(input);
 
-    let input = include_str!("day07/input.txt");
-    day07::solve_part1(input);
-    day07::solve_part2(input);
+    // let input = include_str!("day07/input.txt");
+    // day07::solve_part1(input);
+    // day07::solve_part2(input);
 
-    let input = include_str!("day08/input.txt");
-    day08::solve_part1(input);
-    day08::solve_part2(input);
+    // let input = include_str!("day08/input.txt");
+    // day08::solve_part1(input);
+    // day08::solve_part2(input);
 
-    let input = include_str!("day09/input.txt");
-    day09::solve_part1(input);
-    day09::solve_part2(input);
+    // let input = include_str!("day09/input.txt");
+    // day09::solve_part1(input);
+    // day09::solve_part2(input);
 
-    let input = include_str!("day10/input.txt");
-    day10::solve_part1(input);
-    day10::solve_part2(input);
+    // let input = include_str!("day10/input.txt");
+    // day10::solve_part1(input);
+    // day10::solve_part2(input);
 
-    let input = include_str!("day11/input.txt");
-    day11::solve_part1(input);
-    day11::solve_part2(input);
+    // let input = include_str!("day11/input.txt");
+    // day11::solve_part1(input);
+    // day11::solve_part2(input);
 
-    let input = include_str!("day12/input.txt");
-    day12::solve_part1(input);
-    day12::solve_part2(input);
+    // let input = include_str!("day12/input.txt");
+    // day12::solve_part1(input);
+    // day12::solve_part2(input);
 
     info!("See you tomorrow!");
 }
