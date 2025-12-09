4c34178243cf3c84944f785db0233d99062cdb00
Matthijs Kok <Matthijs.Kok@rabobank.nl>
2025-12-09 11:11:42 +0100

    day 9 part 1 - `itertools.array_combinations()` is pretty but 5x slower

--- 2025/rust/src/day09/mod.rs
+++ 2025/rust/src/day09/mod.rs
@@ -1,23 +1,16 @@
+use itertools::Itertools;
+
 #[tracing::instrument(skip_all, ret)]
 pub fn solve_part1(input: &str) -> usize {
     // Answer = 4725826296
-    let points_vec = input
+    input
         .lines()
         .map(|line| {
             let (l, r) = line.split_once(',').unwrap();
             (l.parse::<u32>().unwrap(), r.parse::<u32>().unwrap())
         })
-        .collect::<Vec<_>>();
-    let points = points_vec.as_slice();
-    let n = points.len();
-    (0..n)
-        .flat_map(|i| {
-            (i + 1..n).map(move |j| {
-                let (x1, y1) = points[i];
-                let (x2, y2) = points[j];
-                (x1.abs_diff(x2) + 1) as usize * (y1.abs_diff(y2) + 1) as usize
-            })
-        })
+        .array_combinations()
+        .map(|[(x1, y1), (x2, y2)]| (x1.abs_diff(x2) + 1) as usize * (y1.abs_diff(y2) + 1) as usize)
         .max()
         .unwrap()
 }
