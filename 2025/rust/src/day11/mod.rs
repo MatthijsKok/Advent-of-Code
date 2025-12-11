dd66fae9c3d517d5675b45f45383dfa82a6a11d2
Matthijs Kok <Matthijs.Kok@rabobank.nl>
2025-12-11 21:27:56 +0100

    day 11 part 1 - cleanup

--- 2025/rust/src/day11/mod.rs
+++ 2025/rust/src/day11/mod.rs
@@ -1,58 +1,30 @@
 use std::collections::HashMap;
 
 // All Device id's are 3 bytes long!
-
-// #[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Hash, Debug, Default)]
-// struct Node([u8; 3]);
-// impl Deref for Node {
-//     type Target = [u8; 3];
-//     #[inline]
-//     fn deref(&self) -> &Self::Target {
-//         &self.0
-//     }
-// }
-// impl From<[u8; 3]> for Node {
-//     fn from(value: [u8; 3]) -> Self {
-//         Self(value)
-//     }
-// }
-// fn parse_node(node: &str) -> Node {
-//     let bytes: [u8; 3] = node.as_bytes().try_into().unwrap();
-//     bytes.into()
-//     // node.as_bytes().try_into().unwrap().into()
-// }
-
-// Maybe a simple type alias is better...
 type Node = [u8; 3];
 
-// const fn parse_node(node: &str) -> Node {
-//     // node.as_bytes().try_into().unwrap()
-//     let b = node.as_bytes();
-//     assert!(b.len() == 3, "node must be a 3 byte string");
-//     [b[0], b[1], b[2]]
-// }
+fn parse_node(node: &str) -> Node {
+    node.as_bytes().try_into().unwrap()
+    // let b = node.as_bytes();
+    // assert!(b.len() == 3, "node must be a 3 byte string");
+    // [b[0], b[1], b[2]]
+}
 
 fn count_paths(current: Node, end: Node, graph: &HashMap<Node, Vec<Node>>) -> usize {
     if current == end {
         return 1;
     }
-
-    let mut total = 0usize;
-    // if let Some(neighbours) = graph.get(&current) {
-    //     for &v in neighbours {
-    //         total += count_paths(v, end, graph);
-    //     }
-    // }
-    let neighbours = graph.get(&current).unwrap();
-    for &n in neighbours {
-        total += count_paths(n, end, graph);
-    }
-    total
+    graph
+        .get(&current)
+        .unwrap()
+        .iter()
+        .map(|&neighbour| count_paths(neighbour, end, graph))
+        .sum()
 }
 
 #[tracing::instrument(skip_all, ret)]
 pub fn solve_part1(input: &str) -> usize {
-    // Answer = ?
+    // Answer = 607
 
     const START: [u8; 3] = *b"you";
     const END: [u8; 3] = *b"out";
@@ -62,13 +34,8 @@ pub fn solve_part1(input: &str) -> usize {
         .map(|line| {
             let (source, destinations) = line.split_once(": ").unwrap();
             (
-                // parse_node(source),
-                source.as_bytes().try_into().unwrap(),
-                destinations
-                    .split_whitespace()
-                    // .map(parse_node)
-                    .map(|d| d.as_bytes().try_into().unwrap())
-                    .collect(),
+                parse_node(source),
+                destinations.split_whitespace().map(parse_node).collect(),
             )
         })
         .collect();
