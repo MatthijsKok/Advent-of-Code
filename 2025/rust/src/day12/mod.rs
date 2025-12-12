1ad1a619dcb93c7872676902790ae75852bd273c
Matthijs Kok <Matthijs.Kok@rabobank.nl>
2025-12-12 13:46:57 +0100

    day 12 WIP

--- 2025/rust/src/day12/mod.rs
+++ 2025/rust/src/day12/mod.rs
@@ -1,3 +1,46 @@
+struct Present {
+    index: u8,
+    shape: [[bool; 3]; 3],
+}
+
+struct Region {
+    dimensions: (u8, u8),
+    desired_presents: [u8; 5],
+}
+
+fn parse_input(input: &str) -> (Vec<Present>, Vec<Region>) {
+    let mut presents = vec![];
+    let mut regions = vec![];
+
+    let mut lines = input.lines();
+    while let Some(line) = lines.next()
+        && !line.contains('x')
+    {}
+
+    for line in lines {
+        // let (dims, prs) = line.split_once(": ").unwrap();
+        // let dimensions = dims.split_once('x').unwrap();
+        // prs
+        line.split_once(": ").map(|(dims, ps)| {
+            Region {
+                dimensions: dims
+                    .split_once('x')
+                    .map(|(d1, d2)| (d1.parse().unwrap(), d2.parse().unwrap()))
+                    .unwrap(),
+                desired_presents: ps
+                    .split_ascii_whitespace()
+                    .map(|p| p.parse::<u8>().unwrap())
+                    .collect::<Vec<_>>()
+                    .try_into()
+                    .unwrap(),
+            }
+            //
+        });
+    }
+
+    (presents, regions)
+}
+
 #[tracing::instrument(skip_all, ret)]
 pub fn solve_part1(input: &str) -> usize {
     // Answer = ?
